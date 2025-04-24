21a54f0dbeabbdcdbe30da31c5870b7df5c6063c
Paul McElhany <paul.mcelhany@noaa.gov>
2025-04-24 09:19:44 -0700

    edit on comments

--- app.R
+++ app.R
@@ -17,10 +17,8 @@ server <- function(input, output, session) {
         )
       
         # copy the quarto generated file to "file" argument.
-        # On local machine, the .pdf file is rendered in the same folder as the .qmd file. 
+        # On a local machine, the .pdf file is rendered in the same folder as the .qmd file. 
         # The copy.file causes the .pdf to copy into the downloads folder.
-        # This might work a bit differently when on the posit server if the 
-        # generated filename/location is different??
         generated_file_name <- "important_report.pdf"
         file.copy(generated_file_name, file)
       }
